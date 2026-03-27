17bc51121c77e1240fc137ab542d7b5eef428ae1
Aleksandar Gjoreski <aleksandar.d.gjoreski@gmail.com>
2026-03-27 01:28:07 +0100

    chore: upgrade to vite 8

--- apps/frontend/vite.config.ts
+++ apps/frontend/vite.config.ts
@@ -6,6 +6,7 @@ import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
 export default defineConfig({
   base: "/themis/",
   resolve: { conditions: ["development"] },
+  build: { chunkSizeWarningLimit: 1536 },
   plugins: [
     TanStackRouterVite({
       routesDirectory: "./src/routes",
