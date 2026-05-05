808f97ac1b51c12a554bfd3a279d0332604ce785
Aleksandar Gjoreski <aleksandar.d.gjoreski@gmail.com>
2026-05-05 21:50:04 +0200

    chore: cleanup, remove ec3 debugger

--- apps/frontend/src/pages/tbd-url/debug/ec3/DebugEc3Route.tsx
+++
@@ -1,9 +0,0 @@
-import { getRouteApi } from "@tanstack/react-router";
-import { DebugEc3RouteContent } from "./DebugEc3RouteContent";
-
-const debugEc3RouteApi = getRouteApi("/tbd-url/debug/ec3");
-
-export function DebugEc3Route() {
-  const { session } = debugEc3RouteApi.useSearch();
-  return <DebugEc3RouteContent session={session} />;
-}
--- apps/frontend/src/pages/tbd-url/debug/ec3/inputContract.ts
+++
@@ -1,8 +0,0 @@
-export { SECTION_CLASS_OPTIONS } from "../../../../features/verifications/ec3/options";
-
-export const ANNEX_EDITABLE_KEYS = [
-  "gamma_M0",
-  "gamma_M1",
-  "lambda_LT_0",
-  "beta_LT",
-] as const;
